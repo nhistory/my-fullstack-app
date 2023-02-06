9cae9634c22c5e4eb0fa91e4daea2ca531cffd40
nhistory <sehwan.lee.dev@gmail.com>
2023-02-05 22:44:39 -0400

    Added form and basic post method

---
+++ app/form.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import React, { useState } from 'react';
+
+export default function FormPost() {
+  const [title, setTitle] = useState('');
+  const router = useRouter();
+  // Create a submit post
+  async function submitPost(e: React.FormEvent) {
+    e.preventDefault();
+    const data = await fetch(`/api/createPost`, {
+      method: 'POST',
+      body: JSON.stringify({ title }),
+    });
+    const res = await data.json();
+    router.refresh();
+    if (!res.ok) console.log(res.message);
+  }
+
+  return (
+    <form onSubmit={submitPost}>
+      <input
+        onChange={(e) => setTitle(e.target.value)}
+        value={title}
+        type="text"
+      />
+      <button type="submit">Make a new post</button>
+    </form>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,23 @@
 import Link from 'next/link';
+import FormPost from './form';
 
-export default function Home() {
+async function getPosts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`);
+  if (!res.ok) {
+    console.log(res);
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const data: { id: number; title: string }[] = await getPosts();
+  console.log(data);
   return (
     <main className="py-4 px-48">
-      <Link
-        className="bg-teal-700 text-black font-medium py-2 px-4 rounded-md"
-        href={'/dashboard'}
-      >
-        Go to the dashboard
-      </Link>
+      <FormPost />
+      {data.map((post) => (
+        <h1 className="text-lg py-6">{post.title}</h1>
+      ))}
     </main>
   );
 }
